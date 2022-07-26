a899158ea2abd78b0d8948d849c5a9e57a7220d1
Park Sungwoo <1998psw@korea.ac.kr>
2022-07-26 17:34:56 +0900

    sw

--- today/settings.py
+++ today/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accountapp.apps.commontoday'
+    'accountapp.apps.accountapptoday'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
