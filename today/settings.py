e93767c69353adfd303cb892de365bf8d299983c
unknown <es4402@korea.ac.kr>
2022-07-26 18:31:18 +0900

    error_please

--- today/settings.py
+++ today/settings.py
@@ -40,7 +40,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accountapp.apps.accountapptoday'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
