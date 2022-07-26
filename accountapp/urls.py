ae474bb2c3b037e1a0afff739032509cf44c07b1
owlwith <owlwithrain@gmail.com>
2022-07-26 18:53:50 +0900

    profileapp implementation

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,7 +3,11 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'accountapp'
 
+
+
+
 urlpatterns = [
+    path('create/', AccountCreateView.as_view(), name="create"),
     path('login/', auth_views.LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accountapp/create.html'
+
+
