913790a28704b9ba115dce8eccf479994309b401
Park Sungwoo <1998psw@korea.ac.kr>
2022-07-26 17:32:11 +0900

    너무 느려

--- today/settings.py
+++ today/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accountapp.apps.commontoday'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
