86f648c1f6266fc9389dac6b2199835054c2593b
owlwith <owlwithrain@gmail.com>
2022-07-29 21:16:31 +0900

    2nd init

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -6,8 +6,6 @@ from accountapp.views import AccountCreateView
 app_name = 'accountapp'
 
 
-
-
 urlpatterns = [
     path('create/', AccountCreateView.as_view(), name="create"),
     path('login/', auth_views.LoginView.as_view(template_name='accountapp/login.html'), name='login'),
