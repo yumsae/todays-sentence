aa4a996fa156b19c8a62d0ad64e560a82184c900
owlwith <owlwithrain@gmail.com>
2022-08-04 21:03:00 +0900

    header update & btn set up

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from profileapp.views import AccountDetailView
 
 app_name = 'profileapp'
 
 urlpatterns = [
-
+    path('detail/', AccountDetailView.as_view(), name="detail"),
+    # path('mypage/', views.Mypage, name='mypage'),
     ]
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,19 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+from django.views.generic.list import MultipleObjectMixin
 
 
+
+class AccountDetailView(DetailView, MultipleObjectMixin):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    # def get_context_data(self, **kwargs):
+    #     object_list = Article.objects.filter(writer=self.get_object())
+    #     return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
