dbe9ac932741a0f3ccb0b6e7dd867cf2ee278cb3
owlwith <owlwithrain@gmail.com>
2022-08-07 01:06:28 +0900

    profile process

--- profileapp/models.py
+++ profileapp/models.py
@@ -9,3 +9,5 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
+
+
