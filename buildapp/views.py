00b41849e53c0a44e4a63ae92456f592d91ecf59
isdiscodead <isdiscodead@korea.ac.kr>
2022-08-07 17:06:21 +0900

    css refactoring

--- buildapp/views.py
+++ buildapp/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'buildapp/home.html')
+
+def write_sentence(request):
+    return render(request, 'buildapp/write_sentence.html')
+
+def write_poet(request):
+    return render(request, 'buildapp/write_poet.html')
