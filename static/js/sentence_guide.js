b7069814aa3399acb6efe03f68574a57a21e424b
unknown <es4402@korea.ac.kr>
2022-08-12 18:06:13 +0900

    guide_slide

--- static/js/sentence_guide.js
+++ static/js/sentence_guide.js
@@ -1,8 +1,8 @@
 (function () {
     const slideList = document.querySelector(".slide_list"); // Slide parent dom
     const slideContents = document.querySelectorAll(".slide_content"); // each slide dom
-    const slideBtnNext = document.querySelector(".fa-solid fa-circle-right icon"); // next button
-    const slideBtnPrev = document.querySelector(".fa-solid fa-circle-left icon"); // prev button
+    const slideBtnNext = document.querySelector(".slide_btn_next"); // next button
+    const slideBtnPrev = document.querySelector(".slide_btn_prev"); // prev button
     const pagination = document.querySelector(".slide_pagination");
     const slideLen = slideContents.length; // slide length
     const slideWidth = 400; // slide width
@@ -21,6 +21,22 @@
     slideList.appendChild(clonedFirst);
     slideList.insertBefore(clonedLast, slideList.firstElementChild);
 
+    // Add pagination dynamically
+    let pageChild = "";
+    for (var i = 0; i < slideLen; i++) {
+        pageChild += '<li class="dot';
+        pageChild += i === startNum ? " dot_active" : "";
+        pageChild += '" data-index="' + i + '"><a href="#"></a></li>';
+    }
+    pagination.innerHTML = pageChild;
+    const pageDots = document.querySelectorAll(".dot"); // each dot from pagination
+
+    slideList.style.transform =
+        "translate3d(-" + slideWidth * (startNum + 1) + "px, 0px, 0px)";
+
+    let curIndex = startNum; // current slide index (except copied slide)
+    let curSlide = slideContents[curIndex]; // current slide dom
+    curSlide.classList.add("slide_active");
 
     /** Next Button Event */
     slideBtnNext.addEventListener("click", function () {
@@ -70,4 +86,24 @@
         pageDots[curIndex].classList.add("dot_active");
     });
 
+    /** Pagination Button Event */
+    let curDot;
+    Array.prototype.forEach.call(pageDots, function (dot, i) {
+        dot.addEventListener("click", function (e) {
+            e.preventDefault();
+            curDot = document.querySelector(".dot_active");
+            curDot.classList.remove("dot_active");
+
+            curDot = this;
+            this.classList.add("dot_active");
+
+            curSlide.classList.remove("slide_active");
+            curIndex = Number(this.getAttribute("data-index"));
+            curSlide = slideContents[curIndex];
+            curSlide.classList.add("slide_active");
+            slideList.style.transition = slideSpeed + "ms";
+            slideList.style.transform =
+                "translate3d(-" + slideWidth * (curIndex + 1) + "px, 0px, 0px)";
+        });
+    });
 })();
