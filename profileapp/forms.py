1054a2a9cdedbd5935e823087e8af28c5a35d7dc
Park Sungwoo <1998psw@korea.ac.kr>
2022-08-19 21:05:11 +0900

    123

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
-from profileapp.models import Profile
 
+from accountapp import forms
+from profileapp.models import Profile
+from django.contrib.auth.forms import UserChangeForm
 
 class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'nickname', 'message']
+
+#class CustomCsUserChangeForm(UserChangeForm):
+#    password = None
+#    hp = forms.IntegerField(label='', widget=forms.NumberInput(
+#        attrs={'class': 'form-control', 'maxlength': '11', 'oninput': "maxLengthCheck(this)", }),
+#    )
--- profileapp/views.py
+++ profileapp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from django.views.generic.list import MultipleObjectMixin
 
+from profileapp.forms import CustomCsUserChangeForm
 from profileapp.models import Profile
 
 
@@ -17,3 +18,16 @@ def MyPage(request):
 def profile_view(request):
     if request.method == 'GET':
         return render(request, 'profileapp/profile.html')
+
+#def profile_update_view(request):
+#    if request.method == 'POST':
+#        user_change_form = CustomCsUserChangeForm(request.POST, instance = request.user)
+#
+        if user_change_form.is_valid():
+            user_change_form.save()
+            return render(request, 'users/profile.html')
+    else:
+        user_change_form = CustomCsUserChangeForm(instance = request.user)
+
+        return render(request, 'users/profile_update.html', {'user_change_form':user_change_form})
+
